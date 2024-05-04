3283ac3bfcb37ed266fdecd6c5a818e79ad6ce86
alfredebi9 <alfredsalvadorfav@gmail.com>
2024-05-04 09:04:25 +0100

    nodemon

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const transporter = nodemailer.createTransport({
   service:'gmail',
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
+    user:process.env.EMAIL_USER,
+    pass:process.env.EMAIL_PASSWORD
   }
 });
 
